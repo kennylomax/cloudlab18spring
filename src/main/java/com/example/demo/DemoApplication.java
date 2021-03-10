067e18dc69e30949da3e43f4812d05a59e54fd6a
kennylomax <kennylomax@gmail.com>
2021-03-10 14:41:22 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		int a;
+		int b=0;
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
